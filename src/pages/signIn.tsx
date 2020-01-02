59e1689bb5f61e690cbef44be1ea2034d8510ee4
Jigar Patel <jigar.moyo@gmail.com>
2020-01-02 16:19:26 -0600

    signature pad working

--- src/pages/signIn.tsx
+++ src/pages/signIn.tsx
@@ -1,67 +1,86 @@
-import React, { FC } from 'react';
-import TextField from '@material-ui/core/TextField';
+// import React, { FC } from 'react';
+// import TextField from '@material-ui/core/TextField'
+// import { makeStyles } from "@material-ui/core/styles";
+// import SignatureCanvas from 'react-signature-canvas';
 
 
-const signIn: FC = () => {
+// const useStyles = makeStyles(theme => ({
+//   root: {
+//     "& > *": {
+//       margin: theme.spacing(5),
+//       width: 600
+//     }
+//   }
+// }));
 
 
-    return (
-        <>
+// const signIn: FC = () => {
 
-      <form noValidate autoComplete="off">
-            <TextField id="filled-basic" label="UIN" variant="filled" />
-      </form>
+//   // eslint-disable-next-line react-hooks/rules-of-hooks
+//   const classes = useStyles();
 
-        </>
-    );
-}
+//     return (
+//         <>
 
-export default signIn;
+
+//       <form className={classes.root} noValidate autoComplete="off">
+
+//         <h2>Sign into event</h2>
+//         <h5>Tap on the UIN textfield and Scan your card. Sign your signature and hit submit</h5>
+
+//         <TextField id="filled-basic" label="UIN" variant="filled" />
+//       </form>      
+
+
+//         </>
+//     );
+// }
+
+// export default signIn;
 
 
 
 // ----------------------------- 
 
-// import React, { Component } from 'react'
-// import ReactDOM from 'react-dom'
-// // import SignaturePad from './react-signature-canvas'
-// // import SignaturePad from 'react-signature-canvas'
-// import styles from './styles.module.css'
+import React, { Component } from 'react'
+import SignatureCanvas from 'react-signature-canvas';
+import styles from './styles.module.css'
 
-// class signIn extends Component {
-//   state = {trimmedDataURL: null}
-//   sigPad = {}
-//   clear = () => {
-//     this.sigPad.clear()
-//   }
-//   trim = () => {
-//     this.setState({trimmedDataURL: this.sigPad.getTrimmedCanvas()
-//       .toDataURL('image/png')})
-//   }
-//   render () {
-//     let {trimmedDataURL} = this.state
-//     return <div className={styles.container}>
-//       <div className={styles.sigContainer}>
-//         <SignaturePad canvasProps={{className: styles.sigPad}}
-//           ref={(ref) => { this.sigPad = ref }} />
-//       </div>
-//       <div>
-//         <button className={styles.buttons} onClick={this.clear}>
-//           Clear
-//         </button>
-//         <button className={styles.buttons} onClick={this.trim}>
-//           Trim
-//         </button>
-//       </div>
-//       {trimmedDataURL
-//         ? <img className={styles.sigImage}
-//           src={trimmedDataURL} />
-//         : null}
-//     </div>
-//   }
-// }
+class signIn extends Component {
+  state = {trimmedDataURL: null}
+  sigPad = {};
+  clear = () => {
+      //@ts-ignore
+      this.sigPad.clear();
+  }
+  trim = () => {
+    //@ts-ignore
+    this.setState({trimmedDataURL: this.sigPad.getTrimmedCanvas()
+      .toDataURL('image/png')})
+  }
+  render () {
 
-// export default signIn;
+    return <div className={styles.container}>
+      <div className={styles.sigContainer}>
+        <SignatureCanvas canvasProps={{className: styles.sigPad}}
+          //@ts-ignore
+          ref={(ref) => { this.sigPad = ref }} />
+      </div>
+      <div>
+        <button className={styles.buttons} onClick={this.clear}>
+          Clear
+        </button>
+        <button className={styles.buttons} onClick={this.trim}>
+          Trim
+        </button>
+      </div>
+
+
+    </div>
+  }
+}
+
+export default signIn;
 
 
 
