669e0e813ede2df1dd7446ff6ca4bddaba5717d7
Bharat Middha <5100938+bmiddha@users.noreply.github.com>
2020-02-01 09:04:19 -0600

    patch sigpad type

--- src/pages/signaturePage.tsx
+++ src/pages/signaturePage.tsx
@@ -6,7 +6,7 @@ import styles from './styles.module.css';
 
 class signaturePage extends Component {
   state = { trimmedDataURL: null };
-  sigPad = {};
+  sigPad: SignatureCanvas | null = null;
   clear = () => {
     //@ts-ignore
     this.sigPad.clear();
